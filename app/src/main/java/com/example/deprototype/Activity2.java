0a8932c7b3cea194283d52e00e96e8be59b5eb54
irene <pwhsieh4@gmail.com>
2019-05-30 17:17:48 -0700

    unfinished prototype

--- app/src/main/java/com/example/deprototype/Activity2.java
+++ app/src/main/java/com/example/deprototype/Activity2.java
@@ -1,13 +1,151 @@
 package com.example.deprototype;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class Activity2 extends AppCompatActivity {
+    private TextView countdownText;
+    private Button countdownButton;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMilliseconds = 14460000;
+    private boolean timerRunning;
+
+    private TextView nextHour1;
+    private TextView nextHour2;
+    private TextView nextHour3;
+
+    private int increment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        countdownText = findViewById(R.id.countdownText);
+        countdownButton = findViewById(R.id.countdownButton);
+
+        if(getIntent().hasExtra("one")) {
+            timeLeftInMilliseconds = 3600000;
+            increment = 1;
+            countdownText.setText("1:00:00");
+        }
+        if(getIntent().hasExtra("two")) {
+            timeLeftInMilliseconds = 3600000*2;
+            increment = 2;
+            countdownText.setText("2:00:00");
+        }
+        if(getIntent().hasExtra("three")) {
+            timeLeftInMilliseconds = 3600000*3;
+            increment = 3;
+            countdownText.setText("3:00:00");
+        }
+        if(getIntent().hasExtra("four")) {
+            timeLeftInMilliseconds = 3600000*4;
+            increment = 4;
+            countdownText.setText("4:00:00");
+        }
+        if(getIntent().hasExtra("five")) {
+            timeLeftInMilliseconds = 3600000*5;
+            increment = 5;
+            countdownText.setText("5:00:00");
+        }
+        if(getIntent().hasExtra("six")) {
+            timeLeftInMilliseconds = 3600000*6;
+            increment = 6;
+            countdownText.setText("6:00:00");
+        }
+
+
+        nextHour1 = findViewById(R.id.nextHour1);
+        nextHour2 = findViewById(R.id.nextHour2);
+        nextHour3 = findViewById(R.id.nextHour3);
+
+        Date date = new Date();
+        String strDateFormat = "HH:mm:ss a";
+        DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
+        final String formattedDate = dateFormat.format(date);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.HOUR, increment   );
+        final String nexthr1 = calendar.getTime()+"";
+        calendar.add(Calendar.HOUR, increment);
+        final String nexthr2 = calendar.getTime()+"";
+        calendar.add(Calendar.HOUR, increment);
+        final String nexthr3 = calendar.getTime()+"";
+
+
+
+
+        countdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startStop();
+                nextHour1.setText(nexthr1);
+                nextHour2.setText(nexthr2);
+                nextHour3.setText(nexthr3);
+            }
+        });
+
     }
+
+    public void startStop(){
+        if(timerRunning){
+            stopTimer();
+        }
+        else{
+            startTimer();
+        }
+    }
+
+    public void startTimer(){
+        countDownTimer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMilliseconds = millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        countdownButton.setText("PAUSE");
+        timerRunning = true;
+    }
+
+    public void stopTimer(){
+        countDownTimer.cancel();
+        countdownButton.setText("START");
+        timerRunning = false;
+    }
+
+    public void updateTimer(){
+        int hours = (int) timeLeftInMilliseconds/3600000;
+        int minutes = (int) timeLeftInMilliseconds/60000;
+        minutes = minutes % 60;
+        int seconds = (int) timeLeftInMilliseconds % 60000 /1000;
+        String timeLeftText;
+        timeLeftText = ""+hours;
+        timeLeftText += ":";
+        if(minutes == 0) timeLeftText += ""+ "00";
+        else timeLeftText += ""+minutes;
+        timeLeftText += ":";
+        if (seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+        countdownText.setText(timeLeftText);
+    }
+
 }
--- app/src/main/java/com/example/deprototype/MainActivity.java
+++ app/src/main/java/com/example/deprototype/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.deprototype;
 
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String timeinterval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,33 +20,83 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner myspinner = (Spinner) findViewById(R.id.spinner1);
-//get the spinner from the xml.
-        //Spinner dropdown = findViewById(R.id.spinner1);
-//create a list of items for the spinner.
-        //String[] items = new String[]{"1", "2", "3", "4", "5", "6"};
-//create an adapter to describe how the items are displayed, adapters are used in several places in android.
-//There are multiple variations of this, but this is the basic variant.
+
+
        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
                getResources().getStringArray(R.array.names));
 
        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         myspinner.setAdapter(myAdapter);
-//set the spinners adapter to the previously created one.
-        //dropdown.setAdapter(adapter);
+
+        myspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+                // On selecting a spinner item
+                String timeMed = parent.getItemAtPosition(pos).toString();
+
+                }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                return;
+            }
+        });
 
         Spinner myspinner2 = (Spinner) findViewById(R.id.spinner);
-//get the spinner from the xml.
-        //Spinner dropdown = findViewById(R.id.spinner1);
-//create a list of items for the spinner.
-        //String[] items = new String[]{"1", "2", "3", "4", "5", "6"};
-//create an adapter to describe how the items are displayed, adapters are used in several places in android.
-//There are multiple variations of this, but this is the basic variant.
+        //myspinner2.setOnItemSelectedListener(this);
+
         ArrayAdapter<String> myAdapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.names));
 
         myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         myspinner2.setAdapter(myAdapter2);
-//set the spinners adapter to the previously created one.
-        //dropdown.setAdapter(adapter);
+
+        myspinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                // On selecting a spinner item
+                timeinterval = parent.getItemAtPosition(pos).toString();
+
+
+                }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                return;
+            }
+        });
+
+
+
+        Button nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextPage = new Intent(getApplicationContext(), Activity2.class);
+                int parsedTime = Integer.parseInt(timeinterval);
+                if(parsedTime == 1) nextPage.putExtra("one", "hehe");
+                if(parsedTime == 2) nextPage.putExtra("two", "hehe");
+                if(parsedTime == 3) nextPage.putExtra("three", "hehe");
+                if(parsedTime == 4) nextPage.putExtra("four", "hehe");
+                if(parsedTime == 5) nextPage.putExtra("five", "hehe");
+                if(parsedTime == 6) nextPage.putExtra("six", "hehe");
+                startActivity(nextPage);
+            }
+        });
+
+
+
+
+
+       /* NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);*/
+
+
     }
 }
